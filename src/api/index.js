0c3cc1674af03068a8adf78d51d58c68068101b8
kusamochi <3056649+kusamochi@users.noreply.github.com>
2019-06-11 17:54:57 +0900

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,9 @@
 import ApiRequest from './ApiRequest';
 
 const expressApi = {
-
   test: {
-    apiTest: () => {
-      return new ApiRequest('post', `/api/test/testApi`);
-    },
+    apiTest: () => new ApiRequest('post', `/api/test/testApi`),
   },
-
 };
 
 export default expressApi;
